2768c394f9edc72202d01620666db09fa4b5a533
Abhishek Agarwal <abhishek4075@gmail.com>
2024-01-03 15:28:42 +0530

    Added: Undo and Redo Functionality

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -8,6 +8,8 @@ function Board() {
     const dispatch = useDispatch();
     const canvasRef = useRef(null);
     const shouldDraw = useRef(false);
+    const drawHistory = useRef([]);
+    const historyPointer = useRef(0);
     const { activeMenuItem, actionMenuItem } = useSelector((state) => state.menu);
     const { color, size } = useSelector((state) => state.toolbox[activeMenuItem])
 
@@ -15,6 +17,7 @@ function Board() {
         if(!canvasRef.current) return;
 
         const canvas = canvasRef.current;
+        const context = canvas.getContext('2d');
 
         if(actionMenuItem == MENU_ITEMS.DOWNLOAD) {
             const URL = canvas.toDataURL();
@@ -22,6 +25,14 @@ function Board() {
             anchor.href = URL;
             anchor.download = 'sketch.jpg';
             anchor.click();
+        } else if (actionMenuItem == MENU_ITEMS.UNDO) {
+            if(historyPointer.current > 0) historyPointer.current -= 1;
+            const imageData = drawHistory.current[historyPointer.current];
+            context.putImageData(imageData, 0, 0);
+        } else if (actionMenuItem == MENU_ITEMS.REDO) {
+            if(historyPointer.current < drawHistory.current.length - 1) historyPointer.current += 1;
+            const imageData = drawHistory.current[historyPointer.current];
+            context.putImageData(imageData, 0, 0);
         }
 
         dispatch(actionItemClick(null));
@@ -51,7 +62,7 @@ function Board() {
 
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
-        
+
         const beginPath = (x, y) => {
             context.beginPath();
             context.moveTo(x, y);
@@ -74,6 +85,9 @@ function Board() {
         
         const handleMouseUp = (e) => {
             shouldDraw.current = false;
+            const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+            drawHistory.current.push(imageData);
+            historyPointer.current = drawHistory.current.length - 1;
         }
 
         canvas.addEventListener('mousedown', handleMouseDown);
